c1b0bbc7f8251c776e88b32df78065c78cb56581
Daniel Lemire <lemire@gmail.com>
2015-10-14 21:17:12 -0400

    Updated the benchmark.

--- README.md
+++ README.md
@@ -3,40 +3,52 @@ Memory usage: HashMap vs TreeMap
 
 Do hash table use more memory than trees? 
 
-
-
 Let us consider Java's HashMap vs. the TreeMap.
 
 ```
-mvn test -Dtest=MemoryBenchmarkTest
+$ mvn -q test
 
-Number of entries: 100
-==============
- Hash size = 6.5546875Kb / tree size = 6.3359375Kb
-==============
+-------------------------------------------------------
+ T E S T S
+-------------------------------------------------------
+objc[17558]: Class JavaLaunchHelper is implemented in both /Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home/jre/bin/java and /Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home/jre/lib/libinstrument.dylib. One of the two will be used. Which one is undefined.
+Running me.lemire.memory.MemoryBenchmarkTest
 
-Number of entries: 1000
 ==============
- Hash size = 69.3671875Kb / tree size = 69.1796875Kb
-==============
-
-Number of entries: 10000
-==============
- Hash size = 687.8671875Kb / tree size = 701.9921875Kb
-==============
-
-Number of entries: 100000
-==============
- Hash size = 7.102409362792969Mb / tree size = 6.865348815917969Mb
+Number of keys (strings) : 100000
+We report the number bytes per key.
+Hash map size = 152.9
+hash table size = 150.3
+tree map size = 150.4
+concurrent hash map size = 152.9
+linked hash map size = 160.9
+skip list size = 146.6
+array size = 118.4
 ==============
 
-Number of entries: 1000000
 ==============
- Hash size = 69.03404998779297Mb / tree size = 68.66344451904297Mb
+Number of keys (integers) : 100000
+We report the number bytes per key.
+Hash map size = 74.5
+hash table size = 71.8
+tree map size = 72.0
+concurrent hash map size = 74.5
+linked hash map size = 82.5
+skip list size = 67.9
+array size = 40.0
 ==============
+For this test, we see that each Integer object takes 20 bytes, 5x what is necessary.
 ```
 
 These results suggest that a TreeMap is no more concise than a HashMap.
+In fact, all Java data structures seem to use nearly an optimal amount
+of memory. The biggest source of waste is the fact that integers
+are wrapped in an Integer object, using 5 times as much memory as they should.
+The String objects also use a lot of memory compared to their content: they 
+could be "packed" much more tightly.
+
+The most "wasteful" data structure appears to be the linked hash map. The most
+concise one is the simple array (which can be considered nearly optimal space wise).
 
 Warning: This test may or may not be reliable. Please review my source
 code for yourself.
--- src/test/java/me/lemire/memory/MemoryBenchmarkTest.java
+++ src/test/java/me/lemire/memory/MemoryBenchmarkTest.java
@@ -1,6 +1,6 @@
 package me.lemire.memory;
 
-import net.sourceforge.sizeof.SizeOf;
+import org.github.jamm.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -8,6 +8,8 @@ import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
 import java.util.*;
+import java.util.concurrent.*;
+import java.text.DecimalFormat;
 
 
 public class MemoryBenchmarkTest {
@@ -15,42 +17,96 @@ public class MemoryBenchmarkTest {
 
     @Test
     public void benchmark() throws Exception {
-        SizeOf.setMinSizeToLog(0);
-        for(int s = 10; s<=1000000; s*=10) {
+        MemoryMeter meter = new MemoryMeter();
+        DecimalFormat df = new DecimalFormat("0.0");
+        for(int s = 100000; s<=100000; s*=10) {
             HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
             TreeMap<Integer,Integer> tree = new TreeMap<Integer,Integer>();
             Hashtable<Integer,Integer> lhash = new Hashtable<Integer,Integer>();
-            int[] arr = new int[s];
+            ConcurrentHashMap<Integer,Integer> chash = new ConcurrentHashMap<Integer,Integer>();
+            LinkedHashMap<Integer,Integer> lihash = new LinkedHashMap<Integer,Integer>();
+            ConcurrentSkipListMap<Integer,Integer> skip = new ConcurrentSkipListMap<Integer,Integer>();
+            Integer[] arr = new Integer[2*s];
 
             for(int k = 0; k < s; ++k) {
                 hash.put(k,k);
                 lhash.put(k,k);
                 tree.put(k,k);
-                arr[k] = k;
+                chash.put(k,k);
+                lihash.put(k,k);
+                skip.put(k,k);
+                arr[2*k] = k;
+                arr[2*k+1] = k;
             }
             System.out.println();
-            System.out.println("Number of entries: "+s);
-
-            System.out.println("Computing hash map size...");
-            String hashsize = SizeOf.humanReadable(SizeOf.deepSizeOf(hash));
-
-            System.out.println("Computing hash table size...");
-           String lhashsize = SizeOf.humanReadable(SizeOf.deepSizeOf(lhash));
-
-            System.out.println("Computing tree map size...");
-            String treesize = SizeOf.humanReadable(SizeOf.deepSizeOf(tree));
-
-            System.out.println("Computing array size...");
-            String arrsize = SizeOf.humanReadable(SizeOf.deepSizeOf(arr));
+            String hashsize = df.format(meter.measureDeep(hash)*1.0/s);
+            String lhashsize = df.format(meter.measureDeep(lhash)*1.0/s);
+            String treesize = df.format(meter.measureDeep(tree)*1.0/s);
+            String arrsize = df.format(meter.measureDeep(arr)*1.0/s);
+            String csize = df.format(meter.measureDeep(chash)*1.0/s);
+            String lisize = df.format(meter.measureDeep(lihash)*1.0/s);
+            String skipsize = df.format(meter.measureDeep(skip)*1.0/s);
 
             System.out.println("==============");
-            System.out.println( " Hash map size = " +hashsize
-                                + " / hash table size = " + lhashsize
-                                + " / tree map size = " + treesize
-                                + " / array size = " + arrsize);
+            System.out.println("Number of keys (integers) : "+s);
+            System.out.println("We report the number bytes per key.");
+            System.out.println( "Hash map size = " +hashsize
+                                + "\nhash table size = " + lhashsize
+                                + "\ntree map size = " + treesize
+                                + "\nconcurrent hash map size = " + csize
+                                + "\nlinked hash map size = " + lisize
+                                + "\nskip list size = " + skipsize
+                                + "\narray size = " + arrsize);
+            System.out.println("==============");
+        }
+        System.out.println("For this test, we see that each Integer object takes 20 bytes, 5x what is necessary.");
+    }
+
+    @Test
+    public void benchmarkString() throws Exception {
+        MemoryMeter meter = new MemoryMeter();
+        DecimalFormat df = new DecimalFormat("0.0");
+        for(int s = 100000; s<=100000; s*=10) {
+            HashMap<String,String> hash = new HashMap<String,String>();
+            TreeMap<String,String> tree = new TreeMap<String,String>();
+            Hashtable<String,String> lhash = new Hashtable<String,String>();
+            ConcurrentHashMap<String,String> chash = new ConcurrentHashMap<String,String>();
+            LinkedHashMap<String,String> lihash = new LinkedHashMap<String,String>();
+            ConcurrentSkipListMap<String,String> skip = new ConcurrentSkipListMap<String,String>();
+            String[] arr = new String[2*s];
+
+            for(int k = 0; k < s; ++k) {
+              hash.put(""+k,""+k);
+              lhash.put(""+k,""+k);
+              tree.put(""+k,""+k);
+              chash.put(""+k,""+k);
+              lihash.put(""+k,""+k);
+              skip.put(""+k,""+k);
+              arr[2*k] = ""+k;
+              arr[2*k+1] = ""+k;
+            }
+            System.out.println();
+            String hashsize = df.format(meter.measureDeep(hash)*1.0/s);
+            String lhashsize = df.format(meter.measureDeep(lhash)*1.0/s);
+            String treesize = df.format(meter.measureDeep(tree)*1.0/s);
+            String arrsize = df.format(meter.measureDeep(arr)*1.0/s);
+            String csize = df.format(meter.measureDeep(chash)*1.0/s);
+            String lisize = df.format(meter.measureDeep(lihash)*1.0/s);
+            String skipsize = df.format(meter.measureDeep(skip)*1.0/s);
+
+            System.out.println("==============");
+            System.out.println("Number of keys (strings) : "+s);
+            System.out.println("We report the number bytes per key.");
+
+            System.out.println( "Hash map size = " +hashsize
+                                + "\nhash table size = " + lhashsize
+                                + "\ntree map size = " + treesize
+                                + "\nconcurrent hash map size = " + csize
+                                + "\nlinked hash map size = " + lisize
+                                + "\nskip list size = " + skipsize
+                                + "\narray size = " + arrsize);
             System.out.println("==============");
         }
     }
-
 
 }
